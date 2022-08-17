1a9548512f46929523faad0a1e96f874fc0a68d1
James Howison <james@howison.name>
2022-08-17 17:33:39 -0500

    parsing all files

--- dwts_scraper/dwts_scraper/spiders/dwts_scores.py
+++ dwts_scraper/dwts_scraper/spiders/dwts_scores.py
@@ -13,14 +13,15 @@ class DwtsScoresSpider(CrawlSpider):
     start_urls = ['https://en.wikipedia.org/wiki/Dancing_with_the_Stars_(American_season_1)']
 
     rules = (
-        Rule(LinkExtractor(allow='wiki/Dancing_with_the_Stars_\\(American_season_1\\)$'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow='wiki/Dancing_with_the_Stars_\\(American_season_\\d+\\)$'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
        # inspect_response(response, self)
         
-        judge_block = response.xpath('//*[@id="Weekly_scores"]')
+        season = response.xpath('string(//*[@id="firstHeading"]/text())').re(r'(\d+)\)$')[0]
         
+        judge_block = response.xpath('//*[@id="Weekly_scores"]')
         judges = judge_block.xpath('../following-sibling::p/i/a/text()').getall()
         
         for week_tag in judge_block.xpath('../following-sibling::h3/span[@class="mw-headline"]'):
@@ -28,11 +29,12 @@ class DwtsScoresSpider(CrawlSpider):
             week = week_tag.xpath('./text()').get()
             
             score_table = week_tag.xpath('../following-sibling::table')[0].get()
-            
             score_pandas = pd.read_html(score_table)[0]
             
-            yield{'week': week,
-                  'judges': judges,
-                  'score_table': score_pandas.to_dict('records')
+            yield{
+                'season': season,
+                'week': week,
+                'judges': judges,
+                'score_table': score_pandas.to_dict('records')
             }
             
