bb3ef354be587fe92d09b97d948db8a4b578ee64
James Howison <james@howison.name>
2022-10-28 16:26:16 -0500

    prior to switching to sibling iteration

--- dwts_scraper/dwts_scraper/spiders/dwts_scores.py
+++ dwts_scraper/dwts_scraper/spiders/dwts_scores.py
@@ -32,12 +32,25 @@ class DwtsScoresSpider(CrawlSpider):
         # Currently this gets all of the judges on the page.  Season 18 somehow has GMA as a judge?
         # Ah, it's a link somewhere in one of these judge sentences.
         # Probably need to get the first preceeding sibling matching for each table
-        # default_judges = weekly_scores.xpath('../following-sibling::p/i/a/text()').getall()
+        
+        if (default_judge_sentences := weekly_scores.xpath('../following-sibling::p[1]')):
+            default_judge_sentence = default_judge_sentences[0] # gets only first judge statement
+           
         
         for week_tag in weekly_scores.xpath('../following-sibling::h3/span[@class="mw-headline"]'):
             
             week = week_tag.xpath('./text()').get()
             
+            # Guest judges are just under the header, if present.
+            if (weekly_judge_candidates := week_tag.xpath("../following-sibling::p[1]/*[contains(text(), 'left to right')]")):
+                judge_html = weekly_judge_candidates[0]
+            else:
+                judge_html = default_judge_sentence
+            
+            judge_sentence = ''.join(judge_html.xpath('./descendant-or-self::*/text()').getall())
+            score_pandas['judge_phrase'] = judge_sentence.split(':')[-1]
+            
+         
             # Get score table, parse using Pandas.read_html which handles rowspan/colspan
             score_table = week_tag.xpath('../following-sibling::table')[0]
 
@@ -48,20 +61,6 @@ class DwtsScoresSpider(CrawlSpider):
             
             score_pandas['season'] = season
             score_pandas['week'] = week
-            
-#             item['score_table'] = score_pandas.to_dict('records') # records gets col = value for each row
-            
-            # Judges are in a sentence just above the table (or a few tables up).
-            judge_html = score_table.xpath('./preceding-sibling::p//*[contains(text(), "Individual judge")]')[-1]
-            # item['judge_sentence'] = ''.join(judge_html.xpath('./descendant-or-self::*/text()').getall())
-            
-            # 'Individual judges scores in the chart below (given in parentheses) are listed in this order 
-            # from left to right: Carrie Ann Inaba, Len Goodman, Robin Roberts, Bruno Tonioli'
-            # item['judges'] = [j.strip(' .') for j in item['judge_sentence'].split(":")[-1].split(", ")]
-            
-            judge_sentence = ''.join(judge_html.xpath('./descendant-or-self::*/text()').getall())
-            score_pandas['judge_phrase'] = judge_sentence.split(':')[-1]
-            # judges = [j.strip(' .') for j in judge_sentence.split(":")[-1].split(", ")]
             
             for row in score_pandas.to_dict('records'):
                 yield row 
